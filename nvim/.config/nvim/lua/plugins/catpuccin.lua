14e9002cf27d0c604dd41a8b2f650fd0e5b0deeb
Tasko <olevski90@gmail.com>
2024-10-10 18:13:53 +0200

    chore: backup before moving to ubuntu

--- nvim/.config/nvim/lua/plugins/catpuccin.lua
+++ nvim/.config/nvim/lua/plugins/catpuccin.lua
@@ -5,11 +5,11 @@ return {
 	opts = {
 		color_overrides = {
 			-- The stuff below makes it so that the mocha background is just black
-			-- mocha = {
-			-- 	base = "#000000",
-			-- 	mantle = "#000000",
-			-- 	crust = "#000000",
-			-- },
+			mocha = {
+				base = "#000000",
+				-- mantle = "#000000",
+				-- crust = "#000000",
+			},
 		},
 	},
 }
