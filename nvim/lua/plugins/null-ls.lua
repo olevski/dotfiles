45cc384ba79333ab4e25c1017beb83190258e0aa
Tasko Olevski <olevski90@gmail.com>
2024-11-10 06:51:22 +0100

    feat: add dmypy

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -7,6 +7,34 @@ return {
 				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.diagnostics.mypy.with({
 					timeout = 600000,
+					command = "dmypy",
+					args = function(params)
+						return {
+							"run",
+							"--timeout",
+							"5000",
+							"--",
+							"--hide-error-codes",
+							"--hide-error-context",
+							"--no-color-output",
+							"--show-absolute-path",
+							"--show-column-numbers",
+							"--show-error-codes",
+							"--no-error-summary",
+							"--no-pretty",
+							"--shadow-file",
+							params.bufname,
+							params.temp_path,
+							params.bufname,
+						}
+					end,
+					runtime_condition = function(params)
+						if string.find(params.bufname, "fugitive") or string.find(params.bufname, ".venv") then
+							return false
+						else
+							return true
+						end
+					end,
 				}),
 				null_ls.builtins.formatting.stylelint,
 			},
