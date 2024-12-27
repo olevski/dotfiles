ecef9221d13b8eff00edb7e568394e914d1aea5d
Tasko Olevski <olevski90@gmail.com>
2024-12-27 03:01:29 +0100

    chore: add ghostty config

--- home.nix
+++ home.nix
@@ -290,6 +290,7 @@
     ".config/wezterm".source = ./wezterm;
     ".config/lazygit".source = ./lazygit;
     ".config/lazydocker".source = ./lazydocker;
+    ".config/ghostty".source = ./ghostty;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
