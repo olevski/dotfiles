3920ac4b67097c4abaf2f1f84e2feb3fb38b048c
Tasko Olevski <olevski90@gmail.com>
2025-02-22 15:32:47 +0100

    feat: add marp and k6

--- home.nix
+++ home.nix
@@ -94,6 +94,8 @@
     pkgs.sops
     pkgs.pack
     pkgs.typst
+    pkgs.k6
+    pkgs.marp-cli
 
     # gs is an existing executable for ghost-script, I want the executable to be called git-spice
     (pkgs.git-spice.overrideAttrs (oldAttrs: {
