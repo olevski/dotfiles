ac67db9ad3bb4c77077eac25ed2c59307ca18114
Tasko Olevski <olevski90@gmail.com>
2025-11-06 18:48:24 +0100

    chore: remove home manager / nix

--- custom-nix-pkgs/default.nix
+++
@@ -1,8 +0,0 @@
-pkgs: {
-  # NOTE: If you do not use this then you have to import nix pkgs in the derivation itself
-  # callPackage takes care of adding things properly
-  # Managed to glean this from:
-  # https://discourse.nixos.org/t/including-local-packages-in-home-manager-configuration/38346
-  # https://github.com/dietmarw/hm-localpkgs
-  poetry-polylith-plugin = pkgs.callPackage ./poetry-polylith-plugin { };
-}
